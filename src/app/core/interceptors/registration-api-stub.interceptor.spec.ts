33eeb03d2319cf20efb7cfff51e79c175701780b
Dmitrij Kuba <dmkuba.dev@gmail.com>
2022-01-26 14:00:15 +0200

    format code

--- src/app/core/interceptors/registration-api-stub.interceptor.spec.ts
+++ src/app/core/interceptors/registration-api-stub.interceptor.spec.ts
@@ -3,14 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { RegistrationApiStubInterceptor } from './registration-api-stub.interceptor';
 
 describe('RegistrationApiStubInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      RegistrationApiStubInterceptor
-      ]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [RegistrationApiStubInterceptor],
+    }),
+  );
 
   it('should be created', () => {
-    const interceptor: RegistrationApiStubInterceptor = TestBed.inject(RegistrationApiStubInterceptor);
+    const interceptor: RegistrationApiStubInterceptor = TestBed.inject(
+      RegistrationApiStubInterceptor,
+    );
     expect(interceptor).toBeTruthy();
   });
 });
